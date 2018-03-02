eee142a394abc80b9e466a77a0c1a329bd09cb55
tunkatunk <jasa4958@colorado.edu>
2018-03-01 23:02:30 -0700

    Initial Commit

---
+++ Fin Fan Design/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fin Fan Design
+//
+//  Created by Jay Tunkie Saunders on 3/1/18.
+//  Copyright © 2018 Jay Tunkie Saunders. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
